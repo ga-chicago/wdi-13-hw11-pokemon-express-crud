de61301049293a0f33daedd91d7974ac1103f2a4
Andy Prudhomme <aprudhomme9@gmail.com>
2018-10-09 17:47:21 -0500

    Commit 2 - Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -6,3 +6,11 @@ const port = 3000;
 app.listen(port, () => {
 	console.log('listening on port ' + port);
 })
+
+const Pokemon = require('./pokemon');
+
+console.log(Pokemon);
+
+app.get('/pokemon', (req, res) => {
+	res.send(Pokemon);
+})
