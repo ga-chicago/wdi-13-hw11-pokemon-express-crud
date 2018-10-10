dda0dce6d0775b332192fff5cd51123d53fb772c
Filip Tatarnik <ftatarnik@gmail.com>
2018-10-09 20:57:44 -0500

    Commit -2 Connected my database. Commit 3 - index.ejs view rendered at pokemon route

--- server.js
+++ server.js
@@ -1,10 +1,43 @@
 const express = require('express');
 const app = express();
-
+const Pokemon = require('./pokemon');
 
 app.get('/', (req, res)=>{
 	res.send('This is my pokemon app')
 });
+
+app.get('/pokemon', (req, res) => {
+    // res.send(Pokemon)
+    res.render('index.ejs', {
+        pokemon: Pokemon
+    })
+    
+});
+
+
+
+
+
+
+app.get('localhost:3000/pokemon', (req,res)=>{
+	res.send('pokemon', {
+		pokemon:Pokemon
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(3000, ()=>{
 	console.log('listening on port 3000');
 });
