ff22154a41f644a3cc510fa973323418a5842b89
Filip Tatarnik <ftatarnik@gmail.com>
2018-10-09 21:25:36 -0500

    Commit 4- show view shows pokemon details, Commit -5 added dynamic anchor tags to index.ejs

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const Pokemon = require('./pokemon');
 
+app.use('/assets', express.static('assets'));
+
 app.get('/', (req, res)=>{
 	res.send('This is my pokemon app')
 });
@@ -14,16 +16,24 @@ app.get('/pokemon', (req, res) => {
     
 });
 
-
-
-
-
-
-app.get('localhost:3000/pokemon', (req,res)=>{
-	res.send('pokemon', {
-		pokemon:Pokemon
+app.get('/pokemon/:id', (req, res)=>{
+	res.render('show.ejs', {
+		pokemon:Pokemon[req.params.id]
 	});
 });
+
+
+
+
+// app.get('/pokemon', (req,res)=>{
+// 	res.send('pokemon', {
+// 		pokemon:Pokemon
+// 	});
+// });
+
+
+
+
 
 
 
