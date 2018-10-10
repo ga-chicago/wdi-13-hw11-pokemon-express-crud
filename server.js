5c63d6a5c87fd492199d85b40a6cf8eb6598b7ae
Josh Hepworth <jrhepworth12@gmail.com>
2018-10-09 22:37:08 -0500

    finished Day 2and added some more styles

--- server.js
+++ server.js
@@ -17,19 +17,39 @@ app.get('/pokemon', (req, res) => {
 	res.render('index.ejs', {
 		pokemon: Pokemon
 	})
-	
+});
+//POST NEW ROUTE
+app.post('/pokemon', (req, res) => {
+	Pokemon.push(req.body)
+	res.redirect('/pokemon')
+})
+//NEW ROUTE
+app.get('/pokemon/new', (req, res) => {
+	res.render('new.ejs')
+});
+//EDIT ROUTE
+app.get('/pokemon/:id/edit', (req, res) => {
+	res.render('edit.ejs', {
+		pokemon: Pokemon[req.params.id],
+		id: req.params.id
+	})
 });
 //SHOW ROUTE
 app.get('/pokemon/:id', (req, res) => {
 	res.render('show.ejs', {
 		pokemon: Pokemon[req.params.id]
 	})
+});
+
+app.delete('/pokemon/:id', (req, res) => {
+	Pokemon.splice(req.params.id, 1);
+	res.redirect('/pokemon')
 })
 
-
-
-
-
+app.put('/pokemon/:id', (req, res) => {
+	Pokemon[req.params.id] = req.body;
+	res.redirect('/pokemon')
+})
 
 
 
