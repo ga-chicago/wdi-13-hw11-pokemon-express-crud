59eccdf5d22c7b835abb49339acf4eb5dea33093
Filip Tatarnik <ftatarnik@gmail.com>
2018-10-10 18:21:41 -0500

    added new pokemon button, GET new, just need to test it w internet

--- server.js
+++ server.js
@@ -16,37 +16,21 @@ app.get('/pokemon', (req, res) => {
     
 });
 
+app.get('/pokemon/new', (req, res) => {
+	res.render('new.ejs')
+})
+
 app.get('/pokemon/:id', (req, res)=>{
 	res.render('show.ejs', {
 		pokemon:Pokemon[req.params.id]
 	});
 });
 
-
-
-
 // app.get('/pokemon', (req,res)=>{
 // 	res.send('pokemon', {
 // 		pokemon:Pokemon
 // 	});
 // });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(3000, ()=>{
 	console.log('listening on port 3000');
