a9d0487bf2dd9c219bba60caf37d403cad740577
Filip Tatarnik <ftatarnik@gmail.com>
2018-10-10 21:14:19 -0500

    everything works but edit. idk why i'm blanking. i've been coding too long all day

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const Pokemon = require('./pokemon');
+const methodOverride = require('method-override');
+
 
 app.use('/assets', express.static('assets'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use(bodyParser.json());
 
-app.get('/', (req, res)=>{
-	res.send('This is my pokemon app')
-});
+// app.get('/', (req, res)=>{
+// 	res.send('This is my pokemon app')
+// });
 
 app.get('/pokemon', (req, res) => {
     // res.send(Pokemon)
@@ -16,9 +22,24 @@ app.get('/pokemon', (req, res) => {
     
 });
 
+app.get('/pokemon/:id/edit', (req, res) => {
+  res.render('edit.ejs', {
+    pokemon: pokemon[req.params.id],
+    i: req.params.index
+  });
+});
+
+
+app.post('/pokemon', (req, res) => {
+	Pokemon.push(req.body);
+	res.redirect('/pokemon');
+})
+
 app.get('/pokemon/new', (req, res) => {
 	res.render('new.ejs')
 })
+
+
 
 app.get('/pokemon/:id', (req, res)=>{
 	res.render('show.ejs', {
@@ -26,11 +47,17 @@ app.get('/pokemon/:id', (req, res)=>{
 	});
 });
 
-// app.get('/pokemon', (req,res)=>{
-// 	res.send('pokemon', {
-// 		pokemon:Pokemon
-// 	});
-// });
+app.delete('/pokemon/:id', (req, res)=>{
+  Pokemon.splice(req.params.id, 1)
+  res.redirect('/pokemon');
+});
+
+app.get('/pokemon', (req,res)=>{
+	res.send('pokemon', {
+		pokemon:Pokemon
+	});
+});
+
 
 app.listen(3000, ()=>{
 	console.log('listening on port 3000');
