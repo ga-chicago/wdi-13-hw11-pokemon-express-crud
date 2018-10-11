e466761f19a700109ba927d5f589dcadc432065a
LehmanBrother <chrislehman1991@gmail.com>
2018-10-10 20:35:11 -0500

    new pokemon

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -10,11 +10,24 @@ router.get('/', (req,res) => {
 	});
 })
 
+//new route
+router.get('/new', (req,res) => {
+	res.render('new.ejs');
+})
+
+//post route
+router.post('/',(req,res) => {
+	Pokemon.push(req.body);
+	res.redirect('/pokemon');
+})
+
 //show route
 router.get('/:id', (req,res) => {
 	res.render('show.ejs', {
 		pokemon: Pokemon[req.params.id]
 	})
 })
+
+
 
 module.exports = router;
